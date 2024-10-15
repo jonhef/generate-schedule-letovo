7c3766468aa7527d37cdd3e92cb5b59828dcf322
Jonhef <jonhefmine@gmail.com>
2024-10-15 10:50:15 +0300

    Add requires and bot

---
+++ src/bot.py
@@ -0,0 +1,14 @@
+import aiogram
+import calendargenerator
+import async_schedule as _schedule
+import datetime
+import asyncio
+
+dp = aiogram.Dispatcher()
+
+async def main():
+    bot = aiogram.Bot("token")
+    
+
+if __name__ == "__main__":
+    asyncio.run(main())
